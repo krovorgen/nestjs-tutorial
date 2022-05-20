5fe7408e1d9c050f6628bbf00d0b9fc5fa051efe
maksim <krovorgen@gmail.com>
2022-05-20 09:18:24 +0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost:27017/nest-course')],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    MongooseModule.forRoot(`mongodb://localhost:27017/${process.env.DB}`),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
